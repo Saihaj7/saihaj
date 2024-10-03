ca08803f64fce23eb77260d614ebbd74ab664e9c
Saihaj Brar <saihaj@saihajs-air.home>
2024-10-03 00:24:55 -0400

    duplicated paragraph to be fixed later 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import Image from "next/image"
+//import Image from "next/image"
 import Link from "next/link"
 import { Github, Linkedin } from 'lucide-react'
 
