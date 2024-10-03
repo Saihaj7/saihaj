8a224d77f6e6a3d4e96aba215025cf078abfd928
Saihaj Brar <saihaj@saihajs-air.home>
2024-10-03 00:17:52 -0400

    early design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,73 @@
-import Image from "next/image";
+'use client'
+
+import { useState, useEffect } from 'react'
+import Image from "next/image"
+import Link from "next/link"
+import { Github, Linkedin } from 'lucide-react'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [text, setText] = useState('')
+  const fullText = `hii there!! i'm an aspiring full stack dev familiar with programming languages such as JavaScript + HTML/CSS, Python, Java, C/C++. i've mainly been doing webdev outside of school using React + Next.js with Postgresql/MongoDB databases.
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+i am currently building MusicDrop and awaiting Spotify's approval for production level API access.
+
+i am currently looking for an entry level dev position!`
+
+  useEffect(() => {
+    let i = 0
+    const typingEffect = setInterval(() => {
+      if (i < fullText.length) {
+        setText(prevText => prevText + fullText.charAt(i))
+        i++
+      } else {
+        clearInterval(typingEffect)
+      }
+    }, 40)
+
+    return () => clearInterval(typingEffect)
+  }, [])
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header className="p-4 bg-gray-100">
+        <nav className="flex justify-center space-x-4">
+          <div className='mr-auto' />
+          <Link href="/" className="text-gray-800 hover:text-gray-600">main</Link>
+          <Link href="/blog" className="text-gray-800 hover:text-gray-600">blog</Link>
+          <Link href="/contact" className="text-gray-800 hover:text-gray-600">projects</Link>
+          <Link href="/contact" className="text-gray-800 hover:text-gray-600">contact</Link>
+        </nav>
+      </header>
+
+      <main className="flex-grow container mx-auto px-4 py-8">
+        <div className="w-3/4 mx-auto">
+          <h1 className="text-2xl font-bold mb-4">
+            saihaj brar
+            <br />
+            toronto, ca
+          </h1>
+          <br />
+          <p className="whitespace-pre-wrap">{text}<span className={`${text?.length % 2 == 0 ? 'inline' : 'hidden'}`}>_</span></p>
+          <br />
+          <p>
+            i am currently building <Link className="bg-black/30 hover:bg-black/50 transition-colors" href="https://musicdrop.vercel.app">MusicDrop</Link> and
+            awaiting Spotify's approval for production level API access.
+          </p>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="p-4 bg-gray-100">
+        <div className="flex justify-center space-x-4">
+          <a href="https://www.linkedin.com/in/saihajbrar/" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-800">
+            <Linkedin size={24} />
+            <span className="sr-only">LinkedIn</span>
+          </a>
+          <a href="https://github.com/Saihaj7" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-gray-800">
+            <Github size={24} />
+            <span className="sr-only">GitHub</span>
+          </a>
+        </div>
       </footer>
     </div>
-  );
+  )
 }
