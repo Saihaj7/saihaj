1449c677a9a9ace1edf9cb504e80ffdb70304f6e
Saihaj Brar <jahias17@gmail.com>
2024-10-04 22:09:03 -0400

    lined bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
       <Navbar />
 
       <main className="flex-grow container mx-auto px-4 py-8">
-
+        <div className="absolute inset-0 -z-10 h-full w-full  bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
         <div className="w-3/4 mx-auto">
 
           <div className=' pb-10'>
